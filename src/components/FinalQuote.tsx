7bb3f09eb397b0b8780253e34301a9fb241c9c4e
JData849 <jrowell@sportingsolutions.com>
2025-10-30 15:22:25 +0000

    __addedforms

--- src/components/FinalQuote.tsx
+++ src/components/FinalQuote.tsx
@@ -27,7 +27,7 @@ export function FinalQuote({ context, phone = "+44XXXXXXXXXX" }: Props) {
 
     // POST to Netlify
     const payload = { "form-name": "quote", ...raw }
-    await fetch("/", {
+    await fetch("/__forms.html", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode(payload),
